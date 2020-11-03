83cdbd5f07634d4daea63f4d96b44e4f356a7ad3
Amanda Killeen <amanda.killeen@gmail.com>
2020-11-02 18:35:35 -0800

    updated models/entry.js

--- models/entry.js
+++ models/entry.js
@@ -1,46 +1,98 @@
-module.exports = function(sequelize, DataTypes) {
-  let Entry = sequelize.define("Entry", {
-    firstname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [1, 255],
-          msg:
-            "Please enter your first name with at least 1 character but no more than 255."
-        }
-      }
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [1, 255],
-          msg:
-            "Please enter your last name with at least 1 character but no more than 255."
-        }
-      }
-    },
-    /*
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      //validate: {}
-    }*/
+module.exports = function (sequelize, DataTypes) {
+    let Entry = sequelize.define("Entry", {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+                len: {
+                    args: [1, 255],
+                    msg:
+                        "Please enter your first name with at least 1 character but no more than 255."
+                }
+            }
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+                len: {
+                    args: [1, 255],
+                    msg:
+                        "Please enter your last name with at least 1 character but no more than 255."
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+                len: {
+                    args: [1, 255],
+                    msg:
+                        "Please enter your email with at least 1 character but no more than 255."
+                }
+            }
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true,
+                msg:
+                    "Please enter a valid date."
+                }
+            },
 
-    airline: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [1, 255],
-          msg:
-              "Please enter your airline with at least 1 character but no more than 255."
+        airline: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 45],
+                    msg: "Please enter an Airline."
+                }
+            }
+        },
+        flightNumber: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true,
+                len: {
+                    args: [1, 45],
+                    msg: "Please enter a Flight Number."
+                }
+            }
+        },
+        illnessName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 45],
+                    msg:
+                        "Please select an illness."
+                }
+            }
+        },
+        symptom_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 255],
+                    msg:
+                        "Please select at least 1 symptom."
+                }
+            }
         }
-      }
-    }
-  });
-  return Entry;
+    }, {
+        freezeTableName: true
+    });
+
+    return Entry;
 };
 
